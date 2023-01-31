650893b786a8b901981338dd01dfd352ae6d9704
vystrukova <strukova.valeria@icloud.com>
2023-01-31 18:01:39 +0300

    api

--- test/info.py
+++ test/info.py
@@ -5,3 +5,7 @@ organization = "Разработчики Автотест"
 text_login = "Логин"
 
 url = "https://mosedo.release133.sedmsk81/"
+
+login_api = "76110320"
+password_api = "Strukova2023!"
+organization_api = "39223722"
