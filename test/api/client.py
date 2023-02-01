83ec43ede84a992d460b3c14b460ec3a5391fd42
vystrukova <strukova.valeria@icloud.com>
2023-02-01 13:48:58 +0300

    debug01

--- test/api/client.py
+++ test/api/client.py
@@ -7,24 +7,36 @@ class ApiClientException(Exception):
     ...
 
 class ApiClient:
-    def __init__(self, base_url: str):
+    def __init__(self, base_url: str, login: str, password: str, organization: str):
         #, login: str, password: str, organization: str
         self.base_url = base_url
 
-        #self.organization = info.organization_api
-        #self.login = info.login_api
-        #self.password = info.password_api
+        self.organization = organization
+        self.login = login
+        self.password = password
 
         #self.session = requests.Session()
 
     def post_login(self):
-        pass
 
-    def get_token(self):
+        headers = {
+            #'set-cookie' : 'auth_token=2823c0a8482d06a048b5f1cdf415291820b13be6'
+        }
+
+        data = {
+            'group_id' : self.organization,
+            'user_id' : self.login,
+            'password' : self.password
+        }
+
+        login_request = requests.post(url='https://mosedo.mos.ru/auth.php?group_id=2', headers=headers, data=data)
+        return login_request
+
+    def get_dnsid(self):
         headers = requests.get(url=self.base_url).headers
         pass
 
 
-c = ApiClient(base_url='https://mosedo.mos.ru/auth.php?uri=%2F')
-c.get_token()
-pass
+c = ApiClient(base_url='https://mosedo.mos.ru/auth.php?group_id=2', organization=info.organization_api, login=info.login_api, password=info.password_api)
+res = c.post_login()
+
